dd4d8ea719a47d09edbf200e1d7dc0186c0db3d2
Caina <cainacc@hotmail.com>
2020-02-12 04:36:13 -0300

    damager and damageable abstract classes

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Damageable : MonoBehaviour
+{
+    [SerializeField] protected float maxHealthPoints;
+    protected float healthPoints;
+
+    public abstract void Damage(Damager damager, float damageAmount);
+
+}
